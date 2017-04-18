ea1c09230bf8cd087546a4da824b2e147da2e0b7
Bassel A. Elerian <basselakmal@gmail.com>
2017-04-18 18:02:17 +0200

    Add files via upload

---
+++ gpa.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>GPA Transcript</title>
+<link type="text/css" rel="stylesheet" href="files/main_stylesheets.css" />
+</head>
+<body>
+
+<table id="MainTable" align="center">
+	
+    <tr>
+    	<td background="files/images/Top.png" bgcolor="darkred" style="background-repeat:no-repeat; background-size:cover; height:10%"><img id="header" src="files/images/gpa_txt.png"></td>
+    </tr>
+    <tr>
+    	<td height="90%">
+        	<div id="Main_Body">
+        
+        	</div>
+        </td>
+    </tr>
+   
+    </table>
+    
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Home</title>
+<link type="text/css" rel="stylesheet" href="files/main_stylesheets.css" />
+</head>
+<body>
+
+<table id="MainTable" align="center" style="padding:4%;">
+	<tr>
+    	<td><a href="timetable.php"><img id="icon" src="files/images/timetable.png" /></a></td>
+        <td class="Hspacer"></td>
+        <td><a href="suggested.php"><img id="icon" src="files/images/suggested.png" /></a></td>
+        <td class="Hspacer"></td>
+        <td><a href="profile.php"><img id="icon" src="files/images/profile.png" /></a></td>
+    </tr>
+    <tr>
+        <td><span id="icon_txt">TimeTable</span></td>
+        <td class="Hspacer"></td>
+        <td><span id="icon_txt">Suggested TimeTable</span></td>
+        <td class="Hspacer"></td>
+        <td><span id="icon_txt">Profile</span></td>
+     </tr>
+     
+     <tr>
+       	<td colspan="5" class="Vspacer"></td>
+     </tr>
+    
+   <tr>
+    	<td><a href="graduation.php"><img id="icon" src="files/images/graduation.png" /></a></td>
+       <td class="Hspacer"></td>
+        <td><a href="gpa.php"><img id="icon" src="files/images/gpa.png" /></a></td>
+        <td class="Hspacer"></td>
+        <td><a href="classwork.php"><img id="icon" src="files/images/classwork.png" /></a></td>
+    </tr>
+    
+    <tr>
+        <td><span id="icon_txt">Graduation Report</span></td>
+        <td class="Hspacer"></td>
+        <td><span id="icon_txt">GPA Transcript</span></td>
+        <td class="Hspacer"></td>
+        <td><span id="icon_txt">Term Classwork</span></td>
+     </tr>
+     
+  	<tr>
+       	<td colspan="5" class="Vspacer"></td>
+     </tr>
+  	
+    
+    <tr>
+    	<td><a href="info.php"><img id="icon" src="files/images/info.png" /></a></td>
+        <td class="Hspacer"></td>
+        <td><a href="announcements.php"><img id="icon" src="files/images/announcements.png" /></a></td>
+        <td class="Hspacer"></td>
+        <td><a href="announcements.php"><img id="icon" src="files/images/announcements.png" /></a></td>
+    </tr>
+    
+    <tr>
+        <td><span id="icon_txt">Info. and Warnings</span></td>
+        <td class="Hspacer"></td>
+        <td><span id="icon_txt">Latest News</span></td>
+        <td class="Hspacer"></td>
+        <td><span id="icon_txt">Latest News</span></td>
+     </tr>
+    
+    
+    <tr>
+    	<td colspan="5"><img src="files/images/LOGO.png" width="60%" /></td>
+    </tr>
+   
+    </table>
+    
+</body>
+</html>
