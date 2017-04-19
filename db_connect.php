908292946338234ce9c9679baa430ee5eec0588d
Bassel A. Elerian <basselakmal@gmail.com>
2017-04-19 21:17:35 +0200

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,4 +1,5 @@
 <?php
-	mysql_connect("localhost:3307","root","usbw") or die("Error while connecting to the datbase!");
+	//mysql_connect("localhost:3307","root","usbw") or die("Error while connecting to the datbase!");
+	mysql_connect("sql208.rf.gd","rfgd_19086182","cufe123") or die("Error while connecting to the datbase!");
 	mysql_select_db("cufe") or die("Error while selecting the table!");
 ?>
