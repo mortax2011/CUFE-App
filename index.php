71fee89e84be70aedb1f3ed75c1ba48367f743e4
Bassel A. Elerian <basselakmal@gmail.com>
2017-04-19 21:13:19 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php include('func.php'); CheckLogin(true); ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -53,7 +54,7 @@
         <td class="Hspacer"></td>
         <td><a href="announcements.php"><img id="icon" src="files/images/announcements.png" /></a></td>
         <td class="Hspacer"></td>
-        <td><a href="announcements.php"><img id="icon" src="files/images/announcements.png" /></a></td>
+        <td><a href="logout.php"><img id="icon" src="files/images/logout.png" /></a></td>
     </tr>
     
     <tr>
@@ -61,7 +62,7 @@
         <td class="Hspacer"></td>
         <td><span id="icon_txt">Latest News</span></td>
         <td class="Hspacer"></td>
-        <td><span id="icon_txt">Latest News</span></td>
+        <td><span id="icon_txt">Logout</span></td>
      </tr>
     
     
