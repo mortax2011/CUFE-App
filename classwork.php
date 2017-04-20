80e584ff089a0678cb2ad93582eb6156a09f1ea8
FadyNasser <fady_nasser9@hotmail.com>
2017-04-20 15:32:30 +0200

    Update classwork.php
    Coursework Task

--- classwork.php
+++ classwork.php
@@ -13,14 +13,47 @@
     <tr>
     	<td id="Header">Term Classwork</td>
     </tr>
-    <tr>
+  <tr>
     	<td height="90%">
         	<div id="Main_Body">
-        
+		<th>Subject Code</th><th>Subject Name</th><th>Midterm</th><th>CourseWork</th>
         	</div>
         </td>
     </tr>
-   
+	<?php
+	
+	include('db_connect.php');	
+	
+	$Student_ID = getID();
+	$query=mysql_query("select Course_Code,Course_Name,Midterm,Classwork from enrolled_in,course where course.Course_ID = enrolled_in.Course_ID && enrolled_in.Student_ID = '".$Student_ID."'") or die('Error while Loading the Grades!');	
+	
+	
+	
+	while($row=mysql_fetch_array($query))
+	{
+		#every iteration in the loop represents a row(tupple) which is included in the '$row' array
+		$Code = $row['Course_Code'];	#Get Data1 from the row and put it in variable '$Data1'
+		$Name= $row['Course_Name'];
+		$Midterm= $row['Midterm'];
+		$Classwork= $row['Classwork'];
+		
+		
+		#for every iteration we are going to create a row (<tr>)
+		#Inside the row, we are going to add both columns (Data1 and Data2)
+		echo('<tr>');
+			
+			
+			echo('<td>'.$Code.'</td>');
+			echo('<td>'.$Name.'</td>');
+			echo('<td>'.$Midterm.'</td>');
+			echo('<td>'.$Classwork.'</td>');
+			
+		echo('</tr>');	
+	}
+	
+	mysql_close();	#Closes the mysql server connection
+	
+?>
     </table>
     
 </body>
