a12019ee4736efbc64cb204ce86e573e01a65668
Bassel A. Elerian <basselakmal@gmail.com>
2017-04-25 19:34:34 +0200

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,4 +1,5 @@
 <?php
 	mysql_connect("sql208.rf.gd","rfgd_19086182","cufe123") or die("Error while connecting to the server!");
 	mysql_select_db("rfgd_19086182_cufe") or die("Error while selecting the database!");
+	mysql_query("set names utf8");
 ?>
