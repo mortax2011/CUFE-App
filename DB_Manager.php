ed2e236e01bee83a3897bf2ee9aad789f9979cb8
Bassel A. Elerian <basselakmal@gmail.com>
2017-04-26 23:56:25 +0300

    Add files via upload

---
+++ DB_Manager.php
@@ -0,0 +1,30 @@
+<?php 
+class DB_Manager
+{
+	public static $connection;
+	public static $con;
+	private static $servername="sql208.rf.gd";
+	private static $username="rfgd_19086182";
+	private static $password="cufe123";
+	private static $dbname="rfgd_19086182_cufe";
+	
+	private function __construct()
+	{
+		self::$con = new mysqli(self::$servername, self::$username, self::$password, self::$dbname) or die("Database connection error!");
+	}
+	
+	public static function Query($SQL)
+	{
+		if(!isset(self::$connection))
+			self::$connection=new DB_Manager;
+		$query=self::$con->query($SQL) or die("Error!");;
+		return $query;
+	}
+	
+	private function __destruct()
+	{
+		mysqli_close(self::$con);
+	}
+}
+
+?>
