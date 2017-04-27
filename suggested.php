3f3aafe7dadccb1c157989324d2be894839dbc41
Bassel A. Elerian <basselakmal@gmail.com>
2017-04-28 00:32:31 +0200

    Update suggested.php

--- suggested.php
+++ suggested.php
@@ -16,7 +16,19 @@
     <tr>
     	<td height="90%">
         	<div id="Main_Body">
-        
+        		<?php 
+				$AvailableCourses=array();
+				$SQL="SELECT C.Course_ID FROM Course C, Offered_For O, Student S WHERE C.Course_ID=O.Course_ID AND S.Student_ID=".getID()." AND S.Program_ID=O.Program_ID AND O.Course_ID Not In (SELECT Course_ID FROM Grades WHERE Student_ID=".getID().");";
+				$query=DB_Manager::Query($SQL);
+				while($row=$query->fetch_assoc())
+				{
+					if(arePassed(getPrerequisites($row['Course_ID'])))
+						array_push($AvailableCourses, $row['Course_ID']);
+				}
+					GenerateSuggested($AvailableCourses, array(0,1, 2, 3, 4, 5))
+				#GenerateSuggested(array(57, 36, 20 , 94, 38, 25, 24, 52), array(1, 2, 3, 4, 5)); 
+
+			?>
         	</div>
         </td>
     </tr>
