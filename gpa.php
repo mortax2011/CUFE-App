962cc8e28bd4c13381b07266fb67f5c7e5ec88c5
Bassel A. Elerian <basselakmal@gmail.com>
2017-05-02 22:53:43 +0200

    Update gpa.php

--- gpa.php
+++ gpa.php
@@ -2,18 +2,10 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<style>
-tr,td,GPATranscript_main,th
-{
-	    border: 1px solid black;
-		color : white ; 
-		align="center"; 
-}
-
-</style>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>GPA Transcript</title>
 <link type="text/css" rel="stylesheet" href="files/main_stylesheets.css" />
+<script src="js_func.js" language="javascript"></script>
 </head>
 <body>
 
